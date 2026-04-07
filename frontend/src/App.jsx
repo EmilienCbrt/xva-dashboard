8e489566137851dd6eee1abe7d2e10af229b3b6e
EmilienCbrt <emiliencombaret@gmail.com>
2026-04-07 17:44:46 +0200

    rectification

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -325,6 +325,7 @@ function App() {
                 }}
               >
                 <h3>Expected Exposure Profile</h3>
+
                 <LineChart width={700} height={300}>
                   <CartesianGrid strokeDasharray="3 3" />
                   <XAxis dataKey="time" />
@@ -348,20 +349,6 @@ function App() {
                     dataKey="value"
                     name="EE-"
                     stroke="#f87171"
-                    strokeWidth={3}
-                    dot={false}
-                  />
-                </LineChart>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="time" />
-                  <YAxis />
-                  <Tooltip />
-                  <Legend />
-                  <Line
-                    type="monotone"
-                    dataKey="value"
-                    name="EE+"
-                    stroke="#60a5fa"
                     strokeWidth={3}
                     dot={false}
                   />
